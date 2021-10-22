1810810a284a425bf5ffe0839600e2fb6164e492
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-22 22:54:44 +0300

    adding project title

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Reviews, Profile, Project
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Reviews)
