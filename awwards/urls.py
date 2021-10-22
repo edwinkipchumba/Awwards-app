f8943fa29df2368798b2ca6bd79a2a330df80325
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-22 21:39:58 +0300

    adding home project review

---
+++ awwards/urls.py
@@ -0,0 +1,28 @@
+"""awwards URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.contrib import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+urlpatterns = [
+    url('admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'',include('projects.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/accounts/login/'}),
+    url(r'^api-token-auth/', obtain_auth_token)
+]
---
+++ base/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
+
+# Create your views here.
+def home(request):
+    projects = Project.get_projects()
+    reviews = Reviews.get_reviews()
+    profile = Profile.get_profile()
+
+    current_user = request.user
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            design = form.cleaned_data['design']
+            usability = form.cleaned_data['usability']
+            content = form.cleaned_data['content']
+            review = form.save(commit=False)
+            review.project = project
+            review.juror = current_user
+            review.design = design
+            review.usability = usability
+            review.content = content
+            review.save()
+        return redirect('home')
+
+    else:
+        form = ReviewForm()
+
+    return render(request,"home.html",{"projects":projects, "reviews":reviews,"form": form,"profile":profile})
